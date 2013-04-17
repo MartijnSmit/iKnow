7fa7bbbd46048759ab7568b72c190bdebb0811b4
martijnsmit <martijn.smit@wemait.nl>
2013-04-17 13:04:13 +0200

    Initial Commit

---
+++ iKnow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iKnow
+//
+//  Created by Martijn Smit on 17-04-13.
+//  Copyright (c) 2013 WeMa IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iKnow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iKnow
+//
+//  Created by Martijn Smit on 17-04-13.
+//  Copyright (c) 2013 WeMa IT. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
